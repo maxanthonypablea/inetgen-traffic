550db52e48a8bc2520dba08286fcab83e29adf32
maxanthony pablea <maxanthonypablea>
2021-01-09 12:12:58 +0800

    file traffic updated

--- traffic.sh
+++ traffic.sh
@@ -7,12 +7,12 @@ vpn_connect() {
 redirect_only() {
     while true :; 
         do cat ~/Documents/scripts/traffic/needle/data/user-agents.txt | while read agents;
-                 do cat ~/Documents/scripts/traffic/needle/data/urls.txt | while read urls;
-                             do cat ~/Documents/scripts/traffic/needle/data/redirector.txt | while read proxy;
-                                             do curl -I -X GET -s $proxy -A $agents -L $urls; done; 
-                                                         done;
-                                                                   done; 
-                                                                       done;
+            do cat ~/Documents/scripts/traffic/needle/data/urls.txt | while read urls;
+                do cat ~/Documents/scripts/traffic/needle/data/redirector.txt | while read proxy;
+                    do curl -I -X GET -s $proxy -A $agents -L $urls; done; 
+                done;
+            done; 
+        done;
 }
 
 proxified_connection () {
@@ -21,11 +21,11 @@ proxified_connection () {
     http_proxy=$(curl -s 'http://pubproxy.com/api/proxy?port=80' | jq '.data[].ipPort' | tr -d '"')
 
     while true :; do
-                agents=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/user-agents.txt)
-                        proxies=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/proxies.txt)
-                                redirect=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/urls.txt)
-                                        curl -I -X GET -A "$agents" -x "$proxies" -L "$redirect";
-                                        done;
+        agents=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/user-agents.txt)
+        proxies=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/proxies.txt)
+        redirect=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/urls.txt)
+        curl -I -X GET -A "$agents" -x "$proxies" -L "$redirect";
+    done;
 }
 
 #vpn_connect
