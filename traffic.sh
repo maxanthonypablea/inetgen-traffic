f8a049f1281a718f852a1c1c53d72d6d0b26fd43
maxanthony pablea <maxanthonypablea>
2021-01-10 14:41:11 +0800

    file updated

--- traffic.sh
+++ traffic.sh
@@ -23,11 +23,28 @@ proxified_connection () {
     while true :; do
         agents=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/user-agents.txt)
         proxies=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/proxies.txt)
-        redirect=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/urls.txt)
-        curl -I -X GET -A "$agents" -x "$proxies" -L "$redirect";
+        urls=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/urls.txt)
+        #curl -I -X GET -A "$agents" -x "$proxies" -L "$urls";
+        curl -I -X POST -A "$agents" -x "$proxies" -L "$urls";
+    done;
+}
+
+
+webproxy_connection () {
+    curl -s 'http://pubproxy.com/api/proxy?port=80&limit=100' | jq '.data[].ipPort' | tr -d '"' > proxies.txt
+    #http_proxy=$(curl -s 'http://pubproxy.com/api/proxy?speed=25&port=80' | jq '.data[].ipPort' | tr -d '"')
+    http_proxy=$(curl -s 'http://pubproxy.com/api/proxy?port=80' | jq '.data[].ipPort' | tr -d '"')
+
+    while true :; do
+        agents=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/user-agents.txt)
+        urls=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/urls.txt)
+        webproxy=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/webproxy.txt)
+        #curl -I -X GET -A "$agents" "$webproxy" -L "$urls";
+        curl -I -X POST -A "$agents" "$webproxy" -L "$urls";
     done;
 }
 
 #vpn_connect
-proxified_connection
+#proxified_connection
+webproxy_connection
 cat /dev/null > proxies.txt
