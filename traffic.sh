ccf0cdd45d484a9e9f5cf1c8bc280e829c756938
maxanthony pablea <maxanthonypablea>
2021-01-11 15:30:07 +0800

    file traffic output updated

--- traffic.sh
+++ traffic.sh
@@ -31,18 +31,22 @@ proxified_connection () {
 
 
 webproxy_connection () {
-    curl -s 'http://pubproxy.com/api/proxy?port=80&limit=100' | jq '.data[].ipPort' | tr -d '"' > proxies.txt
-    #http_proxy=$(curl -s 'http://pubproxy.com/api/proxy?speed=25&port=80' | jq '.data[].ipPort' | tr -d '"')
-    http_proxy=$(curl -s 'http://pubproxy.com/api/proxy?port=80' | jq '.data[].ipPort' | tr -d '"')
-
     while true :; do
         agents=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/user-agents.txt)
         urls=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/urls.txt)
         webproxy=$(shuf -n 1 ~/Documents/scripts/traffic/needle/data/webproxy.txt)
-        #curl -I -X GET -A "$agents" "$webproxy" -L "$urls";
-        curl -I -X POST -A "$agents" "$webproxy" -L "$urls";
+        #curl -s -I -X GET -A "$agents" "$webproxy" -L "$urls";
+        time=$(date +%T)
+        status=$(curl -s -I -X POST -A "$agents" "$webproxy" -L "$urls" 2>/dev/null | head -n1 | awk {'print $2'}; sleep 1;)
+        if (( $status == 302 )); then
+            echo -e "[\033[35m$time\033[0m](\033[32m!GOOD\033[0m) traffic generated now redirecting : \033[36m$urls\033[0m";
+        else 
+                echo -e "[\033[35m$time\033[0m](\033[31m!ERROR\033[0m) check internet connection, webproxy and urls, bugged!";
+        fi
+    
     done;
 }
+
 
 #vpn_connect
 #proxified_connection
